0cfb7fb4fdeab17162ebf2545f68f2a368eabb60
eliseev-eag <eliseev-eag@yandex.ru>
2018-08-18 17:16:09 +0700

    removed unused resources generated default by create-react-app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import Timeline from 'react-visjs-timeline';
 import { loadEvents } from './ducks';
-import logo from './logo.svg';
-import './App.css';
 
 const options = {
   align: 'center',
@@ -17,7 +15,6 @@ const options = {
   },
   snap: null,
   orientation: { axis: 'both' },
-  dataAttributes: ['id'],
   zoomMin: 1000 * 60 * 60 * 24 * 5,
   // max: new Date(),
   // min: new Date(100, 0, 0),
@@ -35,45 +32,9 @@ class App extends Component {
 
     return (
       <div className="App" >
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         {events && events.length > 0 &&
           <Timeline options={options} items={events} groups={eventTypes} />
         }
-        {/* {eventTypes &&
-          <React.Fragment>
-            <h1>Event types:</h1>
-            <ol>
-              {eventTypes.map(eventType => <li key={eventType.id}>{eventType.type}</li>)}
-            </ol>
-          </React.Fragment>
-        }
-        {toponyms &&
-          <React.Fragment>
-            <h1>Toponyms:</h1>
-            <ol>
-              {toponyms.map(toponym => <li key={toponym.id}>{toponym.name}</li>)}
-            </ol>
-          </React.Fragment>
-        }
-        {persons &&
-          <React.Fragment>
-            <h1>Persons:</h1>
-            <ol>
-              {persons.map(person => <li key={person.id}>{`${person.surname} ${person.name} ${person.patron}`}</li>)}
-            </ol>
-          </React.Fragment>
-        }
-        {events && eventTypes &&
-          <React.Fragment>
-            <h1>Events:</h1>
-            <ol>
-              {events.map(event => <li key={event.id}>{`${event.name} ${event.startDate} - ${event.endDate} ${eventTypes.find(p => p.id === event.type).type}`}</li>)}
-            </ol>
-          </React.Fragment>
-        } */}
       </div>
     );
   }
@@ -81,7 +42,7 @@ class App extends Component {
 
 export default connect(
   state => ({
-    events: state.eventsData.events.slice(0, 150).map(event => ({
+    events: state.eventsData.events.filter(event => event.type !== 1).map(event => ({
       start: new Date(event.startDate),
       end: new Date(event.endDate),
       content: event.name,
