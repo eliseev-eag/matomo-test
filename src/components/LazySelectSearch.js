39d79612d666fb43a6880130daeca0fd657b3d4b
Eliseev Alexey <eliseev-eag@yandex.ru>
2018-08-26 23:26:43 +0700

    fixed LazySelectSearch

--- src/components/LazySelectSearch.js
+++ src/components/LazySelectSearch.js
@@ -1,63 +1,86 @@
-import React, { Component } from 'react';
-import { Select } from 'antd';
+import React, { Component } from "react";
+import { uniq } from "lodash-es";
+import { Select } from "antd";
 
 class LazySelectSearch extends Component {
-    static defaultProps = {
-        count: 5,
-        keySelector: option => option.id,
-        valueSelector: option => option.id,
-        nameSelector: option => option.name
-    }
+  static defaultProps = {
+    count: 5,
+    keySelector: option => option.id,
+    valueSelector: option => option.id,
+    nameSelector: option => option.name
+  };
 
-    state = {
-        value: this.props.value,
-        filteredOptions: []
-    }
+  static getDerivedStateFromProps(props) {
+    return { value: props.value };
+  }
 
-    onSearch = value => {
-        const { allOptions, nameSelector } = this.props;
-        const filteredOptions = allOptions.filter(
-            option => nameSelector(option).toLowerCase().includes(value.toLocaleLowerCase()
-            ));
-        this.setState({ filteredOptions });
-    }
+  state = {
+    value: this.props.value,
+    filteredOptions: []
+  };
 
-    onChange = value => {
-        debugger;
-        this.setState({ filteredOptions: [], value });
-        this.props.onChange(value);
-    }
+  onSearch = value => {
+    const { allOptions, nameSelector } = this.props;
+    const filteredOptions = allOptions.filter(option =>
+      nameSelector(option)
+        .toLowerCase()
+        .includes(value.toLocaleLowerCase())
+    );
+    this.setState({ filteredOptions });
+  };
 
-    render() {
-        debugger;
-        const { allOptions, count, keySelector, valueSelector, nameSelector, placeholder } = this.props;
-        const { value, filteredOptions } = this.state;
-        const options = filteredOptions.length ? filteredOptions : [...allOptions.slice(0, count), ...value];
-        const hidedOptionsCount = allOptions.length - options.length;
+  onChange = value => {
+    this.setState({ filteredOptions: [], value });
+    this.props.onChange(value);
+  };
 
-        return (
-            <Select
-                placeholder={placeholder}
-                mode="multiple"
-                filterOption={false}
-                onSearch={this.onSearch}
-                onChange={this.onChange}
-            >
-                {options.map(option =>
-                    <Select.Option
-                        key={keySelector(option)}
-                        value={valueSelector(option)}>
-                        {nameSelector(option)}
-                    </Select.Option>
-                )}
-                {hidedOptionsCount > 0 &&
-                    <Select.Option key='__hidedOptionsCount__' value='__hidedOptionsCount__' disabled>
-                        Еще {hidedOptionsCount} вариантов
-                    </Select.Option>
-                }
-            </Select>
-        );
-    }
+  render() {
+    const {
+      allOptions,
+      count,
+      keySelector,
+      valueSelector,
+      nameSelector,
+      placeholder
+    } = this.props;
+    const { value = [], filteredOptions } = this.state;
+    const mappedValues = value.map(v =>
+      allOptions.find(p => valueSelector(p) === v)
+    );
+    const options = filteredOptions.length
+      ? filteredOptions
+      : uniq([...allOptions.slice(0, count), ...mappedValues]);
+    const hidedOptionsCount = allOptions.length - options.length;
+
+    return (
+      <Select
+        placeholder={placeholder}
+        mode="multiple"
+        filterOption={false}
+        onSearch={this.onSearch}
+        onChange={this.onChange}
+        value={value}
+      >
+        {options.map(option => (
+          <Select.Option
+            key={keySelector(option)}
+            value={valueSelector(option)}
+          >
+            {nameSelector(option)}
+          </Select.Option>
+        ))}
+        {hidedOptionsCount > 0 && (
+          <Select.Option
+            key="__hidedOptionsCount__"
+            value="__hidedOptionsCount__"
+            disabled
+          >
+            Еще {hidedOptionsCount} вариантов
+          </Select.Option>
+        )}
+      </Select>
+    );
+  }
 }
 
 export { LazySelectSearch };
