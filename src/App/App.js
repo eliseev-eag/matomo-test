55b55b96ae811fc216a64b358b8c742c7101ef55
Eliseev Aleksei <eliseev-eag@yandex.ru>
2019-12-04 18:31:09 +0700

    Обернул Menu в Sider

--- src/App/App.js
+++ src/App/App.js
@@ -8,40 +8,53 @@ import { DataViewPage } from "./DataViewPage";
 import { DataEditorPage } from "./DataEditorPage";
 import { editorDataPage, viewDataPage } from "./routes";
 class App extends Component {
+  state = {
+    collapsed: true
+  };
+
   componentDidMount() {
     this.props.actions.loadEvents();
   }
 
+  onCollapse = collapsed => {
+    this.setState({ collapsed });
+  };
+
   render() {
-    const { Content } = Layout;
+    const { collapsed } = this.state;
+    const { Content, Sider } = Layout;
     const routes = [viewDataPage, editorDataPage];
     const selectedMenuKeys = [
       routes.find(route => route === this.props.location.pathname)
     ];
 
     return (
-      <Layout>
-        <Menu mode="horizontal" selectedKeys={selectedMenuKeys}>
-          <Menu.Item key={viewDataPage}>
-            <Link to={viewDataPage}>
-              <Icon type="pie-chart" />
-              Data View
-            </Link>
-          </Menu.Item>
-          <Menu.Item to={editorDataPage}>
-            <Link to={editorDataPage}>
-              <Icon type="edit" />
-              Data Editor
-            </Link>
-          </Menu.Item>
-        </Menu>
-        <Content>
-          <Switch>
-            <Route path={viewDataPage} component={DataViewPage} />
-            <Route path={editorDataPage} component={DataEditorPage} />
-            <Redirect to={viewDataPage} />
-          </Switch>
-        </Content>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider collapsible collapsed={collapsed} onCollapse={this.onCollapse}>
+          <Menu theme="dark" mode="inline" selectedKeys={selectedMenuKeys}>
+            <Menu.Item key={viewDataPage}>
+              <Link to={viewDataPage}>
+                <Icon type="pie-chart" />
+                <span>Data View</span>
+              </Link>
+            </Menu.Item>
+            <Menu.Item key={editorDataPage}>
+              <Link to={editorDataPage}>
+                <Icon type="edit" />
+                <span>Data Editor</span>
+              </Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout>
+          <Content>
+            <Switch>
+              <Route path={viewDataPage} component={DataViewPage} />
+              <Route path={editorDataPage} component={DataEditorPage} />
+              <Redirect to={viewDataPage} />
+            </Switch>
+          </Content>
+        </Layout>
       </Layout>
     );
   }
