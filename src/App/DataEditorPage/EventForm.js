42f741ff58c3633ca8f2952ddecbc3723d2a621d
Eliseev Aleksei <eliseev-eag@yandex.ru>
2019-12-05 16:16:48 +0700

    Переписал компонент на хуки

--- src/App/DataEditorPage/EventForm.js
+++ src/App/DataEditorPage/EventForm.js
@@ -1,8 +1,13 @@
-import React, { PureComponent } from "react";
-import { connect } from "react-redux";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
 import { pick } from "lodash-es";
 import { Form, Input, Row, DatePicker, Select, Drawer, Button } from "antd";
 import { LazySelectSearch } from "../../components/LazySelectSearch";
+import {
+  eventTypesSelector,
+  toponymsSelector,
+  personsSelector
+} from "../../selectors";
 
 const dateFormat = "DD.MM.YYYY";
 
@@ -27,142 +32,130 @@ const rules = {
   }
 };
 
-class EventForm extends PureComponent {
-  componentDidUpdate(prevProps) {
-    const { form, event } = this.props;
-    if (event !== prevProps.event) {
-      const fieldsValues = pick(event, Object.keys(rules));
-      form.setFieldsValue(fieldsValues);
-    }
-  }
+const EventForm = ({
+  event,
+  onSubmit,
+  form: { getFieldDecorator, setFieldsValue, validateFields },
+  visible,
+  onClose
+}) => {
+  const eventTypes = useSelector(eventTypesSelector);
+  const toponyms = useSelector(toponymsSelector);
+  const persons = useSelector(personsSelector);
 
-  onSubmit = event => {
-    const { form, onSubmit } = this.props;
+  const onSubmitHandler = event => {
     event.preventDefault();
-    form.validateFields((err, values) => {
+    validateFields((err, values) => {
       if (!err) {
         onSubmit(values);
       }
     });
   };
 
-  render() {
-    const {
-      form: { getFieldDecorator },
-      visible,
-      onClose,
-      eventTypes,
-      persons,
-      toponyms
-    } = this.props;
+  useEffect(() => {
+    setFieldsValue(pick(event, Object.keys(rules)));
+  }, [event, setFieldsValue]);
 
-    return (
-      <Drawer
-        visible={visible}
-        onClose={onClose}
-        placement="left"
-        width={450}
-        closable
-      >
-        <Form layout="vertical">
-          <Row>
-            <Form.Item label="Название">
-              {getFieldDecorator(
-                "name",
-                rules["name"]
-              )(<Input placeholder="Название" />)}
-            </Form.Item>
-          </Row>
-          <Row>
-            <Form.Item label="Дата начала">
-              {getFieldDecorator(
-                "startDate",
-                rules["startDate"]
-              )(<DatePicker format={dateFormat} />)}
-            </Form.Item>
-          </Row>
-          <Row>
-            <Form.Item label="Дата окончания">
-              {getFieldDecorator(
-                "endDate",
-                rules["endDate"]
-              )(<DatePicker format={dateFormat} />)}
-            </Form.Item>
-          </Row>
-          <Row>
-            <Form.Item label="Тип">
-              {getFieldDecorator(
-                "type",
-                rules["type"]
-              )(
-                <Select placeholder="Выберите тип события">
-                  {eventTypes.map(eventType => (
-                    <Select.Option key={eventType.id} value={eventType.id}>
-                      {eventType.type}
-                    </Select.Option>
-                  ))}
-                </Select>
-              )}
-            </Form.Item>
-          </Row>
-          <Row>
-            <Form.Item label="Топонимы">
-              {getFieldDecorator(
-                "toponyms",
-                rules["toponyms"]
-              )(
-                <LazySelectSearch
-                  allOptions={toponyms}
-                  placeholder="Выберите топонимы"
-                />
-              )}
-            </Form.Item>
-          </Row>
-          <Row>
-            <Form.Item label="Действующие лица">
-              {getFieldDecorator(
-                "persons",
-                rules["persons"]
-              )(
-                <LazySelectSearch
-                  allOptions={persons}
-                  nameSelector={p => `${p.surname} ${p.name} ${p.patron}`}
-                  placeholder="Выберите действующих лиц"
-                />
-              )}
-            </Form.Item>
-          </Row>
-          <div
-            style={{
-              position: "absolute",
-              right: 0,
-              bottom: 0,
-              width: "100%",
-              borderTop: "1px solid #e9e9e9",
-              padding: "10px 16px",
-              background: "#fff",
-              textAlign: "right"
-            }}
-          >
-            <Button onClick={onClose} style={{ marginRight: 8 }}>
-              Cancel
-            </Button>
-            <Button onClick={this.onSubmit} type="primary">
-              Submit
-            </Button>
-          </div>
-        </Form>
-      </Drawer>
-    );
-  }
-}
+  return (
+    <Drawer
+      visible={visible}
+      onClose={onClose}
+      placement="left"
+      width={450}
+      closable
+    >
+      <Form layout="vertical">
+        <Row>
+          <Form.Item label="Название">
+            {getFieldDecorator(
+              "name",
+              rules["name"]
+            )(<Input placeholder="Название" />)}
+          </Form.Item>
+        </Row>
+        <Row>
+          <Form.Item label="Дата начала">
+            {getFieldDecorator(
+              "startDate",
+              rules["startDate"]
+            )(<DatePicker format={dateFormat} />)}
+          </Form.Item>
+        </Row>
+        <Row>
+          <Form.Item label="Дата окончания">
+            {getFieldDecorator(
+              "endDate",
+              rules["endDate"]
+            )(<DatePicker format={dateFormat} />)}
+          </Form.Item>
+        </Row>
+        <Row>
+          <Form.Item label="Тип">
+            {getFieldDecorator(
+              "type",
+              rules["type"]
+            )(
+              <Select placeholder="Выберите тип события">
+                {eventTypes.map(eventType => (
+                  <Select.Option key={eventType.id} value={eventType.id}>
+                    {eventType.type}
+                  </Select.Option>
+                ))}
+              </Select>
+            )}
+          </Form.Item>
+        </Row>
+        <Row>
+          <Form.Item label="Топонимы">
+            {getFieldDecorator(
+              "toponyms",
+              rules["toponyms"]
+            )(
+              <LazySelectSearch
+                allOptions={toponyms}
+                placeholder="Выберите топонимы"
+              />
+            )}
+          </Form.Item>
+        </Row>
+        <Row>
+          <Form.Item label="Действующие лица">
+            {getFieldDecorator(
+              "persons",
+              rules["persons"]
+            )(
+              <LazySelectSearch
+                allOptions={persons}
+                nameSelector={p => `${p.surname} ${p.name} ${p.patron}`}
+                placeholder="Выберите действующих лиц"
+              />
+            )}
+          </Form.Item>
+        </Row>
+        <div
+          style={{
+            position: "absolute",
+            right: 0,
+            bottom: 0,
+            width: "100%",
+            borderTop: "1px solid #e9e9e9",
+            padding: "10px 16px",
+            background: "#fff",
+            textAlign: "right"
+          }}
+        >
+          <Button onClick={onClose} style={{ marginRight: 8 }}>
+            Cancel
+          </Button>
+          <Button onClick={onSubmitHandler} type="primary">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </Drawer>
+  );
+};
 
 const WrappedEventForm = Form.create()(EventForm);
 
-const ConnectedWrappedEventForm = connect(state => ({
-  eventTypes: state.eventTypes,
-  toponyms: state.toponyms,
-  persons: state.persons
-}))(WrappedEventForm);
-
-export { ConnectedWrappedEventForm as EventForm };
+export { WrappedEventForm as EventForm };
