5eb8c2ed1e3f14370e0b421b4934210a7732062a
Eliseev Aleksei <eliseev-eag@yandex.ru>
2019-12-05 16:12:21 +0700

    Добавил redux селекторы в selectors.js

---
+++ src/selectors.js
@@ -0,0 +1,4 @@
+export const eventsSelector = state => state.events;
+export const toponymsSelector = state => state.toponyms;
+export const personsSelector = state => state.persons;
+export const eventTypesSelector = state => state.eventTypes;
