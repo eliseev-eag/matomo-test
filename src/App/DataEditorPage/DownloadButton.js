ea9e2cc9669b6e108c8b3ab6cab6a101053f519a
Eliseev Aleksei <eliseev-eag@yandex.ru>
2019-12-05 18:26:35 +0700

    Переписал компонент DownloadButton на хуки

--- src/App/DataEditorPage/DownloadButton.js
+++ src/App/DataEditorPage/DownloadButton.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Button, Icon } from 'antd';
+import React from "react";
+import { useSelector } from "react-redux";
+import { Button, Icon } from "antd";
+import {
+  eventTypesSelector,
+  eventsSelector,
+  personsSelector,
+  toponymsSelector
+} from "../../selectors";
 
-const DownloadButton = (props) => {
-    const content = JSON.stringify({
-        events: props.events,
-        eventTypes: props.eventTypes,
-        toponyms: props.toponyms,
-        persons: props.persons
-    });
+const DownloadButton = ({ filename = "events.json" }) => {
+  const events = useSelector(eventsSelector);
+  const eventTypes = useSelector(eventTypesSelector);
+  const persons = useSelector(personsSelector);
+  const toponyms = useSelector(toponymsSelector);
 
-    return (
-        <Button download={props.filename}
-            href={URL.createObjectURL(new Blob([content], { type: 'application/json' }))}>
-            <Icon type="download" />
-            Export to json
+  const content = JSON.stringify({
+    events,
+    eventTypes,
+    toponyms,
+    persons
+  });
+
+  return (
+    <Button
+      download={filename}
+      href={URL.createObjectURL(
+        new Blob([content], { type: "application/json" })
+      )}
+    >
+      <Icon type="download" />
+      Export to json
     </Button>
-    );
-}
+  );
+};
 
-DownloadButton.defaultProps = {
-    filename: 'events.json'
-}
-
-const connectedButton = connect(state => ({
-    events: state.events,
-    eventTypes: state.eventTypes,
-    toponyms: state.toponyms,
-    persons: state.persons
-}))(DownloadButton);
-
-export { connectedButton as DownloadButton }
+export { DownloadButton };
