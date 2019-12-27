d78d25790a3b4feaeb5ab37652a3e847408c2fbf
eliseev-eag <eliseev-eag@users.noreply.github.com>
2019-12-27 06:05:18 +0000

    Deploying to gh-pages from master 8386ba9235137340d89f6a547f8c496338de129d

---
+++ precache-manifest.3048cb3e1e1141fa3d7d364d16b5a4dd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28f11923af82040dbe4e0964af547434",
+    "url": "/Viz-react/index.html"
+  },
+  {
+    "revision": "0a603cad5016efd11e1d",
+    "url": "/Viz-react/static/css/2.f2799c73.chunk.css"
+  },
+  {
+    "revision": "8657693aad9708c3481a",
+    "url": "/Viz-react/static/css/main.2d9e4167.chunk.css"
+  },
+  {
+    "revision": "0a603cad5016efd11e1d",
+    "url": "/Viz-react/static/js/2.009dff8e.chunk.js"
+  },
+  {
+    "revision": "54ffdd319393c7b00a30",
+    "url": "/Viz-react/static/js/3.61c3eb89.chunk.js"
+  },
+  {
+    "revision": "8657693aad9708c3481a",
+    "url": "/Viz-react/static/js/main.114e84c7.chunk.js"
+  },
+  {
+    "revision": "57873379cb0d9c400b95",
+    "url": "/Viz-react/static/js/runtime-main.c297699a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Viz-react/precache-manifest.4935458b6d83fc9e93fc2f78424a7e54.js"
+  "/Viz-react/precache-manifest.3048cb3e1e1141fa3d7d364d16b5a4dd.js"
 );
 
 self.addEventListener('message', (event) => {
