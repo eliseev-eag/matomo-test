ddc1436604802c704b33d9f8157da1dd96b97c49
Eliseev Aleksei <eliseev-eag@yandex.ru>
2019-12-30 10:48:08 +0700

    Переписал селекторы на useMemo, чтобы убрать лишние рендеры

--- src/App/DataViewPage/EventsTimeline.js
+++ src/App/DataViewPage/EventsTimeline.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { flow } from 'lodash-es';
 import Timeline from 'react-visjs-timeline';
 import { eventsSelector, eventTypesSelector } from 'selectors';
+
+const MILLISECONDS_IN_DAY = 1000 * 60 * 60 * 24;
 
 const TIMELINE_OPTIONS = {
   align: 'center',
@@ -15,33 +16,37 @@ const TIMELINE_OPTIONS = {
   },
   snap: null,
   orientation: { axis: 'both' },
-  zoomMin: 1000 * 60 * 60 * 24 * 5,
+  zoomMin: MILLISECONDS_IN_DAY * 5,
 };
 
-const eventTypesToGroupSelector = flow(eventTypesSelector, eventTypes =>
-  eventTypes.map(eventType => ({
-    id: eventType.id,
-    content: eventType.type,
-  })),
-);
-
-const eventsToTimelineItemsSelector = flow(eventsSelector, events =>
-  events.slice(0, 10).map(event => ({
-    start: event.startDate,
-    end: event.endDate,
-    content: event.name,
-    duration: event.endDate - event.startDate,
-    group: event.type,
-    type:
-      event.endDate - event.startDate <= 1000 * 60 * 60 * 24
-        ? 'point'
-        : 'range',
-  })),
-);
-
 const EventsTimeline = () => {
-  const items = useSelector(eventsToTimelineItemsSelector);
-  const groups = useSelector(eventTypesToGroupSelector);
+  const events = useSelector(eventsSelector);
+  const eventTypes = useSelector(eventTypesSelector);
+
+  const items = useMemo(
+    () =>
+      events.slice(0, 10).map(event => ({
+        start: event.startDate,
+        end: event.endDate,
+        content: event.name,
+        duration: event.endDate - event.startDate,
+        group: event.type,
+        type:
+          event.endDate - event.startDate <= MILLISECONDS_IN_DAY
+            ? 'point'
+            : 'range',
+      })),
+    [events],
+  );
+
+  const groups = useMemo(
+    () =>
+      eventTypes.map(eventType => ({
+        id: eventType.id,
+        content: eventType.type,
+      })),
+    [eventTypes],
+  );
 
   return (
     <div>
